a3f8d35c0d7cf4a7f10e229579672ee4cc9620aa
Andrew Stuart <andrew.stuart2@gmail.com>
2015-07-06 20:16:20 -0700

    Add basic dir capabilities

--- main.go
+++ main.go
@@ -26,33 +26,49 @@ func (gf *GistFs) GetAttr(n string, c *fuse.Context) (*fuse.Attr, fuse.Status) {
 
 	if gf.files != nil {
 		if f, ok := gf.files[n]; ok {
-			return &fuse.Attr{Mode: fuse.S_IFREG | 0440, Size: uint64(f.Size)}, fuse.OK
+
+			a := &fuse.Attr{Mode: fuse.S_IFREG | 0440, Size: uint64(f.Size)}
+
+			if f.isDir {
+				a.Mode = fuse.S_IFDIR | 0550
+			}
+
+			return a, fuse.OK
 		}
 	}
 
-	return &fuse.Attr{Mode: fuse.S_IFREG | 0770}, fuse.OK
+	return nil, fuse.ENOENT
 }
 
 func (gf *GistFs) OpenDir(n string, c *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
 
 	dirs := make([]fuse.DirEntry, 0)
-	if gf.files != nil {
+	if gf.files != nil && n == "" {
 		for _, f := range gf.files {
-			dirs = append(dirs, fuse.DirEntry{Name: f.Name, Mode: fuse.S_IFREG | 0440})
+			d := fuse.DirEntry{Name: f.Name, Mode: fuse.S_IFREG | 0440}
+			if f.isDir {
+				d.Mode = fuse.S_IFDIR | 0550
+			}
+			dirs = append(dirs, d)
 		}
 		return dirs, fuse.OK
 	}
 
 	gf.files = make(map[string]File)
 
-	gists, err := getGists(gf.user)
+	u := gf.user
+	if n != "" {
+		u = n
+	}
+
+	gists, err := getGists(u)
 	if err != nil {
 		log.Println(err)
 		return nil, fuse.ENOENT
 	}
 	for _, g := range gists {
 		for _, f := range g.Files {
-			gf.files[f.Name] = f
+			gf.files[n+f.Name] = f
 			dirs = append(dirs, fuse.DirEntry{Name: f.Name, Mode: fuse.S_IFREG | 0440})
 		}
 	}
@@ -66,6 +82,8 @@ func getGists(n string) ([]Gist, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	fmt.Printf("res.Header = %+v\n", res.Header)
 
 	dec := json.NewDecoder(res.Body)
 	gists := make([]Gist, 0, 10)
@@ -84,9 +102,16 @@ type Gist struct {
 }
 
 type File struct {
-	RawUrl string `json:"raw_url"`
-	Name   string `json:"filename"`
-	Size   int
+	RawUrl   string `json:"raw_url"`
+	Name     string `json:"filename"`
+	Size     int
+	contents []byte
+	isDir    bool
+}
+
+func (gf *GistFs) Mkdir(n string, mode uint32, c *fuse.Context) fuse.Status {
+	gf.files[n] = File{Name: n, isDir: true}
+	return fuse.OK
 }
 
 func (gf *GistFs) Open(n string, _ uint32, c *fuse.Context) (nodefs.File, fuse.Status) {
@@ -96,6 +121,12 @@ func (gf *GistFs) Open(n string, _ uint32, c *fuse.Context) (nodefs.File, fuse.S
 	}
 
 	if f, ok := gf.files[n]; ok {
+
+		if f.contents != nil {
+			//Cached copy
+			return nodefs.NewDataFile(f.contents), fuse.OK
+		}
+
 		res, err := http.Get(f.RawUrl)
 		defer res.Body.Close()
 		if err != nil {
@@ -108,6 +139,8 @@ func (gf *GistFs) Open(n string, _ uint32, c *fuse.Context) (nodefs.File, fuse.S
 			log.Println(err)
 			return nil, fuse.ENOENT
 		}
+
+		f.contents = bs
 
 		return nodefs.NewDataFile(bs), fuse.OK
 	}
